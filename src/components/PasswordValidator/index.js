0241c8f10b44416e9136a615599176272934c5a7
Siva Dinesh .K <ksd.ccbp4@gmail.com>
2024-04-23 04:58:41 +0000

    coding completed

--- src/components/PasswordValidator/index.js
+++ src/components/PasswordValidator/index.js
@@ -1,1 +1,53 @@
-// Write your code here
+// importing hooks
+import {useState} from 'react'
+
+// importing styled components
+import {
+  MainContainer,
+  ContentCard,
+  Aligner,
+  Title,
+  Description,
+  Password,
+  ErrMsg,
+} from './styledComponents'
+
+const PasswordValidator = () => {
+  const [password, newPassword] = useState('')
+  const [showErr, toggleErr] = useState(true)
+
+  const validatePassword = event => {
+    newPassword(even.target.value)
+    if (event.target.value.length >= 8) {
+      toggleErr(false)
+    } else {
+      toggleErr(true)
+    }
+  }
+
+  return (
+    <MainContainer>
+      <ContentCard>
+        <Aligner>
+          <Title>Password Validator</Title>
+          <Description>
+            Check how strong and secure is your password
+          </Description>
+        </Aligner>
+        <Aligner>
+          <Password
+            type="password"
+            name="password"
+            onChange={validatePassword}
+            value={password}
+          />
+          {showErr && (
+            <ErrMsg>Your password must be at least 8 characters</ErrMsg>
+          )}
+        </Aligner>
+      </ContentCard>
+    </MainContainer>
+  )
+}
+
+export default PasswordValidator
--- src/components/PasswordValidator/styledComponents.js
+++ src/components/PasswordValidator/styledComponents.js
@@ -1,1 +1,74 @@
-// Style your elements here
+import styled from 'styled-components'
+
+// Styling main container
+export const MainContainer = styled.div`
+  background-image: linear-gradient(to bottom, #24263c, #383a4e);
+  height: 100vh;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+// styling content card
+export const ContentCard = styled.div`
+  background: #475569;
+  width: 290px;
+  padding: 5vh 3vw;
+  border-radius: 13px;
+
+  box-shadow: 0 4px 12px 0 #434451;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 13px;
+
+  @media (min-width: 768px) {
+    width: 330px;
+  }
+`
+export const Aligner = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+// Styling Heading and Text content
+export const Title = styled.h2`
+  font-family: 'Roboto';
+  color: #ffffff;
+  margin-bottom: 7px;
+`
+
+export const Description = styled.p`
+  font-family: 'Roboto';
+  color: #f8fafc;
+  font-size: 12px;
+  font-weight: 500;
+`
+export const ErrMsg = styled(Description)`
+  color: #ef4444;
+  font-size: 9px;
+  margin-top: 2px;
+  text-align: center;
+`
+
+// Styling input Element.
+export const Password = styled.input`
+  height: 33px;
+  width: 95%;
+
+  border: none;
+  border-radius: 7px;
+  padding-left: 11px;
+  background-color: #edeeff;
+
+  font-family: 'Roboto';
+  font-size: 13px;
+  font-weight: 500;
+  color: #383a4e;
+  outline: none;
+  cursor: pointer;
+`
